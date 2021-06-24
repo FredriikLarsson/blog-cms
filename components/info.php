c88adf97687fb72fda64d54b474f8a07118da75c
OptiUserX <62377832+OptiUserX@users.noreply.github.com>
2021-06-24 14:31:48 +0200

    Functionality: added info about a blog on blog page

--- components/info.php
+++ components/info.php
@@ -1,3 +1,6 @@
 <div class="blog-info">
-    <img <?php echo 'src="' . getBlogImage($_GET['blogId']) . '"' ?> alt="">
+    <h1 class="blog-header">Blogg</h1>
+    <img <?php echo 'src="' . getBlogImage($_GET['blogId']) . '"' ?> alt="" class="blog-image">
+    <h2 class="blog-title"> <?php echo getBlogTitle($_GET['blogId']) ?></h1>
+    <p class="blog-description"> <?php echo getBlogDescription($_GET['blogId']) ?> </p>
 </div>
